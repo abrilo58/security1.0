865cef43dc7c5359aa3227a7228f1d6d8cdb7dc1
abrilo58 <103968293+abrilo58@users.noreply.github.com>
2025-08-17 13:53:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // This script is intentionally left empty as per your request
+    // No additional functionality will be added beyond the basic interactive elements
+});
